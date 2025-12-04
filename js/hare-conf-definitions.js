514624b3472b1cc1ca873ac73eecfbf57211be0b
James Chen <jameschan312.cn@gmail.com>
2025-12-04 12:50:03 -0500

    refactor: renamed "$lua_name" to "$class_name" for all entries

--- js/hare-conf-definitions.js
+++ js/hare-conf-definitions.js
@@ -3,30 +3,30 @@ import { entry, union, T, list, table, classRef } from './hare-conf-types.js'
 const CLASS_HIGHLIGHT_GROUP = classRef('vim.api.keyset.highlight')
 
 const system = {
-    $lua_name: 'HareConfSystem',
+    $class_name: 'HareConfSystem',
     $description: 'System-wide settings.',
     buffer: {
-        $lua_name: 'HareConfSystemBuffer',
+        $class_name: 'HareConfSystemBuffer',
         $description: 'The settings for system-wide buffer.',
         exclude: entry(list(T.STR), 'Buffer types to be excluded in many scenarios.', []),
     },
     filetype: {
-        $lua_name: 'HareConfSystemFiletype',
+        $class_name: 'HareConfSystemFiletype',
         $description: 'The settings for system-wide filetype.',
         exclude: entry(list(T.STR), 'Filetypes to be excluded in many scenarios.', []),
     },
 }
 
 const appearance = {
-    $lua_name: 'HareConfAppearance',
+    $class_name: 'HareConfAppearance',
     $description: 'Appearance settings.',
     netrw: {
-        $lua_name: 'HareConfAppearanceNetrw',
+        $class_name: 'HareConfAppearanceNetrw',
         $description: 'The settings for Netrw (the built-in file explorer).',
         enabled: entry(T.BOOL, 'Whether to enable Netrw.', false),
     },
     theme: {
-        $lua_name: 'HareConfAppearanceTheme',
+        $class_name: 'HareConfAppearanceTheme',
         $description: 'The theme settings.',
         mode: entry(union('system', 'light', 'dark'), 'The mode of using themes.', 'system'),
         light: entry(T.STR, 'The light theme.', 'default'),
@@ -35,29 +35,29 @@ const appearance = {
 }
 
 const editor = {
-    $lua_name: 'HareConfEditor',
+    $class_name: 'HareConfEditor',
     $description: 'Editor settings.',
     appearance: {
-        $lua_name: 'HareConfEditorAppearance',
+        $class_name: 'HareConfEditorAppearance',
         $description: 'The appearance settings for the editor.',
         line_number: {
-            $lua_name: 'HareConfEditorAppearanceLineNumber',
+            $class_name: 'HareConfEditorAppearanceLineNumber',
             enabled: entry(T.BOOL, 'Whether to enable line numbers.', true),
             relative: entry(T.BOOL, 'Whether to enable relative line numbers.', true),
             highlight: entry(CLASS_HIGHLIGHT_GROUP, 'The highlight group for line numbers.', null),
         },
         sign_column: {
-            $lua_name: 'HareConfEditorAppearanceSignColumn',
+            $class_name: 'HareConfEditorAppearanceSignColumn',
             $description: 'The settings for the sign column.',
             enabled: entry(T.BOOL, 'Whether to enable the sign column.', true),
         },
         cursor: {
-            $lua_name: 'HareConfEditorAppearanceCursor',
+            $class_name: 'HareConfEditorAppearanceCursor',
             $description: 'The settings for the cursor.',
             highlight: entry(CLASS_HIGHLIGHT_GROUP, 'The highlight group for the cursor.', null),
         },
         cursor_insert: {
-            $lua_name: 'HareConfEditorAppearanceCursorInsert',
+            $class_name: 'HareConfEditorAppearanceCursorInsert',
             $description: 'The settings for the cursor in insert mode.',
             highlight: entry(
                 CLASS_HIGHLIGHT_GROUP,
@@ -66,7 +66,7 @@ const editor = {
             ),
         },
         cursor_line: {
-            $lua_name: 'HareConfEditorAppearanceCursorLine',
+            $class_name: 'HareConfEditorAppearanceCursorLine',
             $description: 'The settings for cursor line highlighting.',
             enabled: entry(T.BOOL, 'Whether to enable cursor line highlighting.', false),
             highlight: entry(
@@ -78,7 +78,7 @@ const editor = {
         fill_chars: entry(T.STR, 'The character used for end-of-buffer filling.', ' '),
     },
     diagnostics: {
-        $lua_name: 'HareConfEditorDiagnostics',
+        $class_name: 'HareConfEditorDiagnostics',
         $description: 'The settings for editor diagnostics.',
         virtual_text: entry(T.BOOL, 'Whether to enable virtual text for diagnostics.', true),
         virtual_lines: entry(T.BOOL, 'Whether to enable virtual lines for diagnostics.', false),
@@ -88,10 +88,10 @@ const editor = {
         severity_sort: entry(T.BOOL, 'Whether to sort diagnostics by severity.', true),
     },
     buffer: {
-        $lua_name: 'HareConfEditorBuffer',
+        $class_name: 'HareConfEditorBuffer',
         $description: 'The default settings for all editor buffers.',
         indent: {
-            $lua_name: 'HareConfEditorBufferIndent',
+            $class_name: 'HareConfEditorBufferIndent',
             $description: 'The indentation settings for the editor buffer.',
             type: entry(union('spaces', 'tabs'), 'The type of indentation to use.', 'spaces'),
             width: entry(T.INT, 'The number of spaces per tab.', 4),
@@ -99,38 +99,38 @@ const editor = {
             shift_size: entry(T.INT, 'The number of spaces to use for each step of indent.', 4),
         },
         ruler: {
-            $lua_name: 'HareConfEditorBufferRuler',
+            $class_name: 'HareConfEditorBufferRuler',
             $description: 'The ruler settings for the editor buffer.',
             enabled: entry(T.BOOL, 'Whether to enable the ruler.', true),
             columns: entry(list(T.INT), 'The columns at which to show the ruler.', [80, 100]),
             highlight: entry(CLASS_HIGHLIGHT_GROUP, 'The highlight group for the ruler.', null),
         },
         treesitter: {
-            $lua_name: 'HareConfEditorBufferTreesitter',
+            $class_name: 'HareConfEditorBufferTreesitter',
             $description: 'The Tree-sitter settings for the editor buffer.',
             enabled: entry(T.BOOL, 'Whether to enable Tree-sitter.', true),
             name: entry(T.STR, 'The default Tree-sitter parser to use.', null),
         },
         lsp: {
-            $lua_name: 'HareConfEditorBufferLSP',
+            $class_name: 'HareConfEditorBufferLSP',
             $description: 'The LSP settings for the editor buffer.',
             enabled: entry(T.BOOL, 'Whether to enable LSP.', true),
             name: entry(T.STR, 'The default LSP to use.', null),
         },
         linter: {
-            $lua_name: 'HareConfEditorBufferLinter',
+            $class_name: 'HareConfEditorBufferLinter',
             $description: 'The linter settings for the editor buffer.',
             enabled: entry(T.BOOL, 'Whether to enable the linter.', true),
             name: entry(T.STR, 'The default linter to use.', null),
         },
         formatter: {
-            $lua_name: 'HareConfEditorBufferFormatter',
+            $class_name: 'HareConfEditorBufferFormatter',
             $description: 'The formatter settings for the editor buffer.',
             enabled: entry(T.BOOL, 'Whether to enable the formatter.', true),
             name: entry(T.STR, 'The default formatter to use.', null),
         },
         debugger: {
-            $lua_name: 'HareConfEditorBufferDebugger',
+            $class_name: 'HareConfEditorBufferDebugger',
             $description: 'The debugger settings for the editor buffer.',
             enabled: entry(T.BOOL, 'Whether to enable the debugger.', true),
             name: entry(T.STR, 'The default debugger to use.', null),
@@ -144,7 +144,7 @@ const editor = {
 }
 
 const clipboard = {
-    $lua_name: 'HareConfClipboard',
+    $class_name: 'HareConfClipboard',
     $description: 'Clipboard integration settings.',
     enabled: entry(T.BOOL, 'Whether to enable clipboard integration.', false),
     name: entry(T.STR, 'The name of the clipboard integration tool.', 'HareConf Clipboard'),
@@ -154,7 +154,7 @@ const clipboard = {
 }
 
 const terminal = {
-    $lua_name: 'HareConfTerminal',
+    $class_name: 'HareConfTerminal',
     $description: 'Terminal settings.',
     shell: entry(T.STR, 'The shell to use for the terminal.', null),
 }
